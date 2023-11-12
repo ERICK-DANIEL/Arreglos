588837fa1041a946268c3f3bae29fa1db5a1a63b
Erick Daniel <erickdanielmartinezmartinez123@gmail.com>
2023-11-11 18:36:23 -0600

    Nuevo archivo al proyecto

---
+++ Ejercicio3.c
@@ -0,0 +1,30 @@
+/* A partir del programa propuesto, que almacenaba en una tabla el número
+de días que tiene cada mes, crear otro que pida al usuario que le indique
+la fecha, detallando el día (1 al 31) y el mes (1=enero, 12=diciembre), como
+respuesta muestre en pantalla el número de días que quedan hasta final de año*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int dia;
+    int diaDelMes;
+    int diaFinal = 0;
+    int mes;
+    int dias[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    printf("Ingesa el dia y el mes:");
+    scanf("%d %d", &dia, &mes);
+
+    diaDelMes = dias[mes - 1] - dia;
+
+    for (int i = mes; i < 12; i++){
+        diaFinal = diaFinal + dias[i];
+    }
+
+    diaFinal = diaFinal + diaDelMes;
+
+    printf("Faltan %d dias para final de año", diaFinal);
+    return 0;
+
+}
